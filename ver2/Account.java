b2237a9e98a46bbf8f125d95c848cc6ca6ba6a37
w-adair <156733385+w-adair@users.noreply.github.com>
2024-01-15 21:21:45 -0500

    ver2 edited

--- ver2/Account.java
+++ ver2/Account.java
@@ -2,7 +2,7 @@ package ver2;
 
 public class Account {
 	// Instance variable, only available inside this class.
-	private double balance;
+	private double balance;//New Comment for balance
 	private String name;
 
 	public Account(double balance) {
